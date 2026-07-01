99eb770250b4cc373e7968b612ac4c10b706a632
tylerrweng <tylerrweng@gmail.com>
2026-07-01 11:29:20 -0700

    fixed preprocess format

--- ml/src/preprocess.py
+++ ml/src/preprocess.py
@@ -1,28 +1,27 @@
-def clean_data(data):
+def clean_data(text):
     # lowercase text, expand contractions, remove punctuation, normalize whitespace,
     # strip leading/trailing whitespace
-    for row in data:
-        # lowercase text
-        row['text'] = row['text'].lower()
-        # expand contractions
-        contractions = {
-            "can't": "cannot",
-            "won't": "will not",
-            "ain't": "is not",
-            "y'all": "you all",
-            "n't": " not",
-            "'re": " are",
-            "'s": " is",
-            "'d": " would",
-            "'ll": " will",
-            "'t": " not",
-            "'ve": " have",
-            "'m": " am"
-        }
-        for contraction, expansion in contractions.items():
-            row['text'] = row['text'].replace(contraction, expansion)
-        # remove punctuation and normalize whitespace
-        row['text'] = ''.join(char for char in row['text'] if char.isalnum() or char.isspace())
-        # strip leading/trailing whitespace
-        row['text'] = ' '.join(row['text'].split())
-    return data
+    # lowercase text
+    text = text.lower()
+    # expand contractions
+    contractions = {
+        "can't": "cannot",
+        "won't": "will not",
+        "ain't": "is not",
+        "y'all": "you all",
+        "n't": " not",
+        "'re": " are",
+        "'s": " is",
+        "'d": " would",
+        "'ll": " will",
+        "'t": " not",
+        "'ve": " have",
+        "'m": " am"
+    }
+    for contraction, expansion in contractions.items():
+        text = text.replace(contraction, expansion)
+    # remove punctuation and normalize whitespace
+    text = ''.join(char for char in text if char.isalnum() or char.isspace())
+    # strip leading/trailing whitespace
+    text = ' '.join(text.split())
+    return text
