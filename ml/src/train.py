7fc832cf664b5c4c3c3624f0de457ee4b44765f5
tylerrweng <tylerrweng@gmail.com>
2026-07-03 11:47:54 -0700

    return udpate

--- ml/src/train.py
+++ ml/src/train.py
@@ -1,19 +1,29 @@
 from load_data import load_imdb_dataset
 from preprocess import clean_text
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 def train_model():
     # load the IMDB dataset
     train_data, test_data = load_imdb_dataset()
+    train_data, dev_data = train_test_split(
+        train_data,
+        test_size=0.2,
+        random_state=42
+    )
 
     # apply the clean_data function to the text data in both train and test datasets
     train_data = train_data.map(lambda row: {"text": clean_text(row["text"])})
+    dev_data = dev_data.map(lambda row: {"text": clean_text(row["text"])})
     test_data = test_data.map(lambda row: {"text": clean_text(row["text"])})
 
     # extract the text and labels from the datasets
     train_text = train_data["text"]
     train_labels = train_data["label"]
+
+    dev_text = dev_data["text"]
+    dev_labels = dev_data["label"]
 
     test_text = test_data["text"]
     test_labels = test_data["label"]
@@ -25,6 +35,7 @@ def train_model():
         ngram_range=(1, 2)
     )
     train_vectors = vectorizer.fit_transform(train_text)
+    dev_vectors = vectorizer.transform(dev_text)
     test_vectors = vectorizer.transform(test_text)
 
     # Training
@@ -34,4 +45,10 @@ def train_model():
         random_state=42
     ).fit(train_vectors, train_labels)
 
-    return reg, vectorizer, test_vectors, test_text, test_labels
+    return (
+        reg,
+        vectorizer,
+        train_vectors, train_data,
+        dev_vectors, dev_data,
+        test_vectors, test_data
+    )
