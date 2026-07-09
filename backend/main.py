264332ecb9e9ba574307735744f5f214de0ecfb3
tylerrweng <tylerrweng@gmail.com>
2026-07-09 11:54:30 -0700

    response handling

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import joblib
 from fastapi import FastAPI, Request
 from ml.src.predict_sentiment import predict_sentiment
+from models import SentimentRequest, SentimentResponse
 
 def startup():
     model = joblib.load("../models/model.joblib")
@@ -17,17 +18,15 @@ app.add_event_handler("startup", startup)
 app.add_event_handler("shutdown", shutdown)
 
 @app.post("/predict")
-async def run_inference(request: Request):
-    # get request and extract text
-    body = (await request.json())
-    input_text = body["text"]
-
+async def predict_route(request: SentimentRequest):
     # call inference pipeline
     prediction, confidence_scores = predict_sentiment(
-        input_text,
+        request.text,
         app.state.model,
         app.state.vectorizer
     )
-
-    # predict proba returns a numpy array, convert to list for JSON serialization
-    return {"prediction": prediction, "confidence_scores": confidence_scores.tolist()}
+    
+    return SentimentResponse(
+        sentiment=prediction,
+        confidence_scores=confidence_scores.tolist()
+    )
