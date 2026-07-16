5825704733d8da7933ce936eb8a8bd371ba15ca3
tylerrweng <tylerrweng@gmail.com>
2026-07-16 00:16:57 -0700

    app

---
+++ frontend/src/app.jsx
@@ -0,0 +1,15 @@
+import SentimentPage from "./pages/SentimentPage"
+
+export default function App() {
+    return (
+        <div className="app-container">
+            <header className="app-header">
+                <h1>Sentiment Analyzer</h1>
+            </header>
+
+            <main className="app-main">
+                <SentimentPage />
+            </main>
+        </div>
+    )
+} 
