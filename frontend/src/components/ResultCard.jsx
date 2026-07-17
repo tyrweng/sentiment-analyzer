0a507f35b12a870d889ccc76eb0639ec81c06a74
tylerrweng <tylerrweng@gmail.com>
2026-07-17 16:01:19 -0700

    fixed card

--- frontend/src/components/ResultCard.jsx
+++ frontend/src/components/ResultCard.jsx
@@ -2,9 +2,7 @@ import React from "react"
 import ConfidenceBar from "./ConfidenceBar";
 
 export default function ResultCard({ sentiment, confidenceScores }) {
-    // const [negativeScore, positiveScore] = confidenceScores;
-    const negativeScore = confidenceScores[0].value
-    const positiveScore = confidenceScores[1].value
+    const [negativeScore, positiveScore] = confidenceScores;
     const confidence = Math.max(negativeScore, positiveScore)
 
     return (
